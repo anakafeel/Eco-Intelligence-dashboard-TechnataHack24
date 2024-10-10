cce7c5f3ffe1b3122d90a834ce6aa5d5381b31ca
saim hashmi <hashmisaim037@gmail.com>
2024-10-10 11:39:04 -0400

    title change

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,8 +4,8 @@ import DefaultLayout from "@/components/Layouts/DefaultLayout";
 
 export const metadata: Metadata = {
   title:
-    "Next.js E-commerce Dashboard | TailAdmin - Next.js Dashboard Template",
-  description: "This is Next.js Home for TailAdmin Dashboard Template",
+    "Eco-Intelligence - User Dashboard",
+  description: "This is the dashboard of the Eco_intelligence Applpication",
 };
 
 export default function Home() {
